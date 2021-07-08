8841b0cac3298d2d59bed9a36a06f3acf2fb202f
FGoecking <flaviogoecking@gmail.com>
2021-07-07 22:38:53 -0300

    função para checar texto dos tweets marcados

--- botcupido.py
+++ botcupido.py
@@ -3,26 +3,36 @@ import tweepy
 import time
 import random
 
+#keys
 api_key = ''
 api_secret_key = ''
-
 access_token = ''
 secret_access_token = ''
 
+#autentificação
 auth = tweepy.OAuthHandler(api_key, api_secret_key)
 auth.set_access_token(access_token, secret_access_token)
 
+#chamada da api
 api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
-search = '#Solteira'
-numeroDeTweets = 10
+procurar = ['#Solteiro', '#Solteira', '#Solteire', '#solteiro']
+p = "solteiro"
+mencoes = api.mentions_timeline()
+print(mencoes[0].text)
 
-for tweet in tweepy.Cursor(api.search, search).items(numeroDeTweets):
-    try:
-        print('tweet retuitado')
-        tweet.retweet()
-        time.sleep(60)
-    except tweepy.TweepError as e:
-        print(e.reason)
-    except StopIteration:
-        break
+def checarTexto(mencoes):
+       for item in procurar:
+        if item in mencoes[0].text:
+            print("achei")
+            return True
+        else:
+            print("nao achei")
+
+def dar_qrt(checarTexto):
+    if checarTexto == True:
+        tweepy.retweet()
+        print("dei rt")
+
+dar_qrt(checarTexto(mencoes))
+
